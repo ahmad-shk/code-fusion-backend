15538b3985a5121fe7026a7972f0b11eca336fb8
Ahmad Saleem <ahmadsaleem19950@gmail.com>
2025-09-20 22:19:49 +0500

    update

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
+// import express from "express";
 const express = require("express");
-const dotenv = require("dotenv");
+const cors = require("cors");
 const contactRoutes = require("../routes/contactRoutes.js");
 
-dotenv.config();
 const app = express();
 
-app.use(express.json()); // 👈 POST body parse karne ke liye
-app.use("/api", contactRoutes);
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+app.use("/api/contact", contactRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(5000, () => console.log("Server running on port 5000"));
